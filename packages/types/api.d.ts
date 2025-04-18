571fc3506a96b55faae090c4b8295f1b29e7f9a1
Fraimer De La Cruz <77998859+fraimerr@users.noreply.github.com>
2025-04-18 09:25:29 -0400

    Update API types

--- packages/types/api.d.ts
+++ packages/types/api.d.ts
@@ -1,5 +1,27 @@
 import type { Users } from "@prisma/client";
 
 export type APIUser = {
-  user: Users
+	user: Users;
+};
+
+export interface Guild {
+	id: string;
+	name: string;
+	icon: string | null;
+	botPresent: boolean;
 }
+
+export interface Module {
+	name: string;
+	enabled: boolean;
+}
+
+export interface APIResponse<T> {
+	success: boolean;
+	message: string;
+	data: T;
+}
+
+export type APIV1UserGuildsResponse = APIResponse<Guild[]>;
+
+export type APIV1ModulesResponse = APIResponse<Module[]>;
